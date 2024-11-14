ecdac2eb3ae52fe6741d30b2a5754eb499f1da1a
Luisa Leon Campos <luisaleon34563@gmail.com>
2024-11-14 17:40:29 +0000

    subiendo proyecto

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+  public static void main(String[] args) {
+ double Precio100Gramos = 5.95;
+
+    double Preciokilo = Precio100Gramos *1000/100;
+
+    System.out.println("El precio del producto es: " + Preciokilo + "€");
+    
+
+  }
+
+}
